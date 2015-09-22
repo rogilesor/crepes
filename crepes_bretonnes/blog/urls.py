5997b9a223a51f4df6b38d7fa7f17a658d3e0519
rogilesor <istarosel@yahoo.fr>
2015-09-22 20:48:48 +0200

    avec variable sans nom dans url

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -1,3 +1,7 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('blog.views',url(r'^accueil$','home'),)
+urlpatterns = patterns('blog.views',
+    url(r'^accueil$','home'),
+    url(r'^article/(\d+)$','view_article'),
+    url(r'^articles/(\d{4})/(\d{2})$','list_articles'),
+)
--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -8,3 +8,9 @@ def home(request):
     return HttpResponse(text)
 
 # Create your views here.
+
+def view_article(request, id_article):
+    """Vue qui affiche un article selon son identifiant (ou ID, ici un numero)
+       Son ID est le second parametre de la fonction (pour rappel, le premier parametre est toujours la requete de l'utilisateur)"""
+    text = "Vous avez demande l'article #{0} !".format(id_article)
+    return HttpResponse(text)
