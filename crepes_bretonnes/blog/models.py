c0c70bc676f262b4000e4ad9253e6a7daaae632a
rogilesor <istarosel@yahoo.fr>
2015-09-24 21:13:11 +0200

    intro model

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+	titre = models.CharField(max_length=100)
+	auteur = models.CharField(max_length=42)
+	contenu = models.TextField(null=True)
+	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+
+	def __str__(self):
+		return self.titre
