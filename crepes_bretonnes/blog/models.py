e33d897853b37004c684ace673df14ebec4aa175
rogilesor <istarosel@yahoo.fr>
2015-09-25 21:15:22 +0200

    ForeignKey

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -6,6 +6,14 @@ class Article(models.Model):
 	auteur = models.CharField(max_length=42)
 	contenu = models.TextField(null=True)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+	categorie = models.ForeignKey('Categorie')
 
 	def __str__(self):
 		return self.titre
+
+class Categorie(models.Model):
+	nom = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.nom
+
