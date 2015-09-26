88823f5cfd7a8179983c7f8124f38e307abf9c79
rogilesor <istarosel@yahoo.fr>
2015-09-26 14:19:04 +0200

    nouvelles colonnes

--- crepes_bretonnes/blog/admin.py
+++ crepes_bretonnes/blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from blog.models import Categorie, Article
 
-admin.site.register(Categorie)
-admin.site.register(Article)
-
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre','categorie','auteur','date')
+    list_filter  = ('auteur','categorie',)
+    date_hierarchy = 'date'
+    ordering = ('date',)
+    search_fields = ('titre','contenu')
+
+admin.site.register(Categorie)
+admin.site.register(Article,ArticleAdmin)
--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
 	categorie = models.ForeignKey('Categorie')
 
-	def __str__(self):
+	def __unicode__(self):
 		return self.titre
 
 class Categorie(models.Model):
