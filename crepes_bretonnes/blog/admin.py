ae9cb3978a159afdfa772d6364b005da59d0bdd3
rogilesor <istarosel@yahoo.fr>
2015-09-26 18:11:29 +0200

    ajout debut post

--- crepes_bretonnes/blog/admin.py
+++ crepes_bretonnes/blog/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 from blog.models import Categorie, Article
 
-# Register your models here.
-
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('titre','categorie','auteur','date')
-    list_filter  = ('auteur','categorie',)
+    list_display   = ('titre', 'categorie', 'auteur', 'date', 'apercu_contenu')
+    list_filter    = ('auteur','categorie', )
     date_hierarchy = 'date'
-    ordering = ('date',)
-    search_fields = ('titre','contenu')
+    ordering       = ('date', )
+    search_fields  = ('titre', 'contenu')
+
+    def apercu_contenu(self, article):
+        text = article.contenu
+        textReduit = article.contenu[0:40]
+        if len(article.contenu) > 40:
+            return '%s…' % textReduit
+        else:
+            return text
+        # return text
+
+    # En-tête de notre colonne
+    apercu_contenu.short_description = 'Aperçu du contenu'
 
 admin.site.register(Categorie)
-admin.site.register(Article,ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
