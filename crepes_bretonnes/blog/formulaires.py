e25ee064822b044b316b39dd6549a16614aecef7
rogilesor <istarosel@yahoo.fr>
2015-09-27 20:49:29 +0200

    ex2 clean formulaire

--- crepes_bretonnes/blog/formulaires.py
+++ crepes_bretonnes/blog/formulaires.py
@@ -6,8 +6,18 @@ class ContactForm(forms.Form):
 	envoyeur = forms.EmailField(label="Votre adresse mail")
 	renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé", required=False)
 
-	def clean_message(self):
-		message = self.cleaned_data['message']
-		if "pizza" in message:
-			raise forms.ValidationError("On ne veut pas entendre parler de pizza !")
-		return message
+	# def clean_message(self):
+	# 	message = self.cleaned_data['message']
+	# 	if "pizza" in message:
+	# 		raise forms.ValidationError("On ne veut pas entendre parler de pizza !")
+	# 	return message
+
+	def clean(self):
+		cleaned_data = super(ContactForm,self).clean()
+		sujet = cleaned_data.get('sujet')
+		message = cleaned_data.get('message')
+
+		if sujet and message :
+			if "pizza" in sujet and "pizza" in message:
+				raise forms.ValidationError("Vous parlez trop des pizzas")
+		return cleaned_data
